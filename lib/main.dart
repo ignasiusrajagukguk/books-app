1f2bf0af711406add41be804bf89849e7ba4f208
ignasiusrajagukguk <ignasius.aritonang@gmail.com>
2024-07-01 15:05:06 +0700

    App Icon : change all the app icons
    refactor : add http.Client register function in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,21 @@ import 'package:books_app/src/common/injection/injector.dart';
 import 'package:books_app/src/common/router/jobbox_router.dart';
 import 'package:books_app/src/presentation/effects/scrolls.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  getIt.registerSingleton<http.Client>(http.Client());
   configureInjections();
+  runApp(const BooksApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BooksApp extends StatelessWidget {
+  const BooksApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Books App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
